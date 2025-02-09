2411993cd36de9e88d964bf371bb09935174d961
Mariam Eid <meid@lab2r1s10.42abudhabi.ae>
2025-02-09 15:23:26 +0400

    first commit

---
+++ color.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Text Colors
+    printf("\033[30m Black \033[0m\n");
+    printf("\033[31m Red \033[0m\n");
+    printf("\033[32m Green \033[0m\n");
+    printf("\033[33m Yellow \033[0m\n");
+    printf("\033[34m Blue \033[0m\n");
+    printf("\033[35m Magenta \033[0m\n");
+    printf("\033[36m Cyan \033[0m\n");
+    printf("\033[37m White \033[0m\n");
+    printf("\033[90m Bright Black \033[0m\n");
+    printf("\033[91m Bright Red \033[0m\n");
+    printf("\033[92m Bright Green \033[0m\n");
+    printf("\033[93m Bright Yellow \033[0m\n");
+    printf("\033[94m Bright Blue \033[0m\n");
+    printf("\033[95m Bright Magenta \033[0m\n");
+    printf("\033[96m Bright Cyan \033[0m\n");
+    printf("\033[97m Bright White \033[0m\n");
+    printf("\033[38;2;255;165;0m Orange \033[0m\n"); 
+
+    // Background Colors
+    printf("\033[40m Black Background \033[0m\n");
+    printf("\033[41m Red Background \033[0m\n");
+    printf("\033[42m Green Background \033[0m\n");
+    printf("\033[43m Yellow Background \033[0m\n");
+    printf("\033[44m Blue Background \033[0m\n");
+    printf("\033[45m Magenta Background \033[0m\n");
+    printf("\033[46m Cyan Background \033[0m\n");
+    printf("\033[47m White Background \033[0m\n");
+    printf("\033[100m Bright Black Background \033[0m\n");
+    printf("\033[101m Bright Red Background \033[0m\n");
+    printf("\033[102m Bright Green Background \033[0m\n");
+    printf("\033[103m Bright Yellow Background \033[0m\n");
+    printf("\033[104m Bright Blue Background \033[0m\n");
+    printf("\033[105m Bright Magenta Background \033[0m\n");
+    printf("\033[106m Bright Cyan Background \033[0m\n");
+    printf("\033[107m Bright White Background \033[0m\n");
+    printf("\033[48;2;255;165;0m Orange Background \033[0m\n");
+
+    // Styles
+    printf("\033[0m Reset \033[0m\n");
+    printf("\033[1m Bold \033[0m\n");
+    printf("\033[4m Underline \033[0m\n");
+    printf("\033[7m Negative \033[0m\n");
+
+    return 0;
+}
